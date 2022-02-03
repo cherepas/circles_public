8fbb4846d448eccdc28a2e3ca443870d9783919c
Vsevolod Cherepashkin <cherepashkin@phystech.edu>
2022-02-03 16:11:52 +0100

    s

--- helpers.py
+++ helpers.py
@@ -153,8 +153,8 @@ def f2p(far, dirs, nsp, vox2mm):
     # @simple_time_tracker(_log)
 def lossfig(dirname, lossar, ylabel, title, xlim, ylim, lb, xlabel):
     plt.rcParams["figure.figsize"] = (9, 5)
-    fig = plt.figure()
-    axes = plt.gca()
+    # fig = plt.figure()
+    # axes = plt.gca()
     if lb == 'pc+f':
         labels_text = [['train 0', 'val 0'], ['train 1', 'val 1']]
         colist = ['C0', 'C1']
@@ -198,12 +198,12 @@ def lossfig(dirname, lossar, ylabel, title, xlim, ylim, lb, xlabel):
     # print( 'wefwef')
     for tp in ['.png', '.pdf']:
         plt.savefig(dirname + ylabel + tp, bbox_inches='tight')
-    try:
-        print(get_ipython().__class__.__name__)
-        plt.show(fig)
-    except:
-        pass
-    plt.close(fig)
+    # try:
+    #     print(get_ipython().__class__.__name__)
+    #     plt.show(fig)
+    # except:
+    #     pass
+    # plt.close(fig)
 
 def lossout(fnm, xlabel, lossar, epp, dirname, lb):
     lossfig(jn(dirname, fnm), lossar,
