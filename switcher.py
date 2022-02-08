fix bug with val aug loss averaging

switcher.py
@@ -4,7 +4,7 @@ import numpy as np
 import os
 import sys
 def out2loss(opt, model, inputs, iscuda, nsp, cbs, y_n2, C, angles_list, lb,
-             vox2mm, GT, loss_fn, moments, phase, dirname, i_batch, epoch):
+             vox2mm, GT, loss_fn, moments, phase, dirname, i_batch, epoch, index):
     if opt.inputt in ('img', 'f') and \
             not lb in ('pc+f', 'pose6', 'eul', 'orient') \
             and not opt.outputt == 'f_n':
@@ -115,10 +115,14 @@ def out2loss(opt, model, inputs, iscuda, nsp, cbs, y_n2, C, angles_list, lb,
     if all([opt.inputt == 'img', opt.outputt in ('orient'),
             lb in ('orient'), opt.aug_gt, phase=='val']):
         outputs = model(inputs)
-        if i_batch == 0:
-            np.savetxt(os.path.join(dirname, 'netOutputs', 'val_output'+\
-                                    '_e'+str(epoch).zfill(3)),
-                       outputs.cpu().detach().numpy(),delimiter=',')
+        # if i_batch == 0:
+        #     np.savetxt(os.path.join(dirname, 'netOutputs', 'val_output'+\
+        #                             '_e'+str(epoch).zfill(3)),
+        #                outputs.cpu().detach().numpy(),delimiter=',')
+        #     np.savetxt(os.path.join(dirname, 'netOutputs', 'val_gt'+\
+        #                             '_e'+str(epoch).zfill(3)),
+        #                GT.cpu().detach().numpy(),delimiter=',')
+        #     print(125, index)
         # print(115)
         # gt4 = t.zeros(gt3.shape[0], 12, 3, 3)
         peridx = np.array([[0, 1, 2], [0, 2, 1], [1, 0, 2],
@@ -131,25 +135,17 @@ def out2loss(opt, model, inputs, iscuda, nsp, cbs, y_n2, C, angles_list, lb,
                           [[-1, 0, 0], [0, +1, 0], [0, 0, -1]]])
         signf = signf.cuda() if iscuda else signf
         # TODO write in vectorize form instead of for loop
-        # augsize = 1
-        # if 'matrix_sign_flip' in opt.aug_gt and not 'vector_sign_flip' in opt.aug_gt:
-        #     augsize *= 2
-        # elif 'vector_sign_flip' in opt.aug_gt:
-        #     augsize *= 6
-        # if 'vector_permute' in opt.aug_gt:
-        #     augsize *= 6
-        # lo = t.zeros(outputs.shape[0], augsize, 9)
         lo = t.zeros(outputs.shape[0], 2, 6, 6, 9)
-        outputs = outputs.reshape(-1,3,3)
+        outputs1 = outputs.reshape(-1,3,3)
         # TODO appearance: use property that vector_sign_flip includes
         #  matrix sign flip, and that they are mutually exclusive.
 
         for i in range(outputs.shape[0]):
             if 'svd' in opt.aug_gt:
-                outputs2 = t.matmul(t.svd(outputs[i, :, :])[0],
-                                    t.svd(outputs[i, :, :])[2].T)
+                outputs2 = t.matmul(t.svd(outputs1[i, :, :])[0],
+                                    t.svd(outputs1[i, :, :])[2].T)
             else:
-                outputs2 = outputs[i, :, :]
+                outputs2 = outputs1[i, :, :]
             for j in range(2):
                 if 'matrix_sign_flip' in opt.aug_gt:
                     outputs3 = (-1) ** (j) * outputs2
@@ -166,42 +162,10 @@ def out2loss(opt, model, inputs, iscuda, nsp, cbs, y_n2, C, angles_list, lb,
                         else:
                             outputs5 = outputs4
                         lo[i, j, k, l, :] = loss_fn(GT[i, :], outputs5.reshape(9))
-        # for j in range(outputs.shape[0]):
-        #     if 'svd' in opt.aug_gt:
-        #         outputs2 = t.matmul(t.svd(outputs[j, :,:])[0], t.svd(outputs[j, :, :])[2].T)
-        #     else:
-        #         outputs2 = outputs[j,:,:]
-        #     for i in range(augsize):
-        #         if 'matrix_sign_flip' in opt.aug_gt:
-        #             outputs3 = (-1) ** (i % 2) * outputs2
-        #         else:
-        #             outputs3 = outputs2
-        #         if 'vector_sign_flip' in opt.aug_gt:
-        #
-        #             outputs4 = t.matmul()
-        #         if 'vector_permute' in opt.aug_gt:
-        #             outputs5 = outputs4[:, peridx[i // 2, :]]
-        #         else:
-        #             outputs5 = outputs4
-        #         lo[j, i, :] = loss_fn(GT[j,:], outputs5.reshape(9))
-        # augopt = ['matrix_sign_flip', 'vector_sign_flip', 'vector_permute', 'svd']
-        # if all([i in opt.aug_gt for i in augopt[0, 2, 3]]):
-        #     lo = t.zeros(outputs.shape[0], augsize, 9)
-        #     for j in range(outputs.shape[0]):
-        #         outputs2 = t.matmul(t.svd(outputs[j, :,:])[0], t.svd(outputs[j, :, :])[2].T)
-        #         for i in range(augsize):
-        #             if 'matrix_sign_flip' in opt.aug_gt:
-        #                 outputs3 = (-1) ** (i % 2) * outputs2
-        #             else:
-        #                 outputs3 = outputs2
-        #             if 'vector_sign_flip' in opt.aug_gt:
-        #                 outputs4 = t.matmul(outputs3, signf)
-        #             if 'vector_permute' in opt.aug_gt:
-        #                 outputs5 = outputs4[:, peridx[i // 2, :]]
-        #             else:
-        #                 outputs5 = outputs4
-        #             lo[j, i, :] = loss_fn(GT[j,:], outputs5.reshape(9))
-        loss = t.mean(t.amin(lo, dim=(1,2,3)))
+        loss = t.mean(t.amin(t.mean(lo, dim=4), dim=(1, 2, 3)))
+        # loss = t.mean(t.amin(lo, dim=(1,2,3)))
+        # if i_batch == 0:
+        #     print(205, 'aug_loss', loss, 'direct loss = ', t.mean(loss_fn(GT, outputs)))
         # loss = t.mean(t.min(loss_fn(outputs, GT), loss_fn(outputs, -GT)))
         outputs_2 = 0
         latent = 0