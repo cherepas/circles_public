acc438060ac60fbe581fb5c83b19ed42391d5eab
cherepashkinVI <cherepashkin@phystech.edu>
2022-02-10 14:11:54 +0100

    w

--- experiments/e074/e074w008.sh
+++ experiments/e074/e074w008.sh
@@ -16,4 +16,4 @@
 #export CUDA_VISIBLE_DEVICES="0,1,2,3"
 #SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-python -u "../../main.py" -datapath "D:/cherepashkin1/phenoseed/" -realjobname `basename "$0"` -jobname `basename "$0"` -jobdir "$SCRIPT_DIR" -expnum "e074" -epoch 1000 -bs 5 -num_input_images 3 -framelim 6000 -criterion "L2" -rmdirname -lr 5e-5 -hidden_dim 32 9 -inputt "img" -outputt "orient" -lb "orient" -no_loadh5 -minmax_fn "" -parallel "torch" -machine "workstation" -merging "batch" -aug_gt "vector_sign_flip" "vector_permute" "svd" -updateFraction 0.25 -steplr 1000 1 -print_minibatch 10 -dfname "598frame"
+python -u "../../main.py" -datapath "D:/cherepashkin1/phenoseed/" -realjobname `basename "$0"` -jobname `basename "$0"` -jobdir "$SCRIPT_DIR" -expnum "e074" -epoch 1000 -bs 5 -num_input_images 3 -framelim 6000 -criterion "L2" -rmdirname -lr 5e-4 -hidden_dim 32 9 -inputt "img" -outputt "orient" -lb "orient" -no_loadh5 -minmax_fn "" -parallel "torch" -machine "workstation" -merging "batch" -aug_gt "vector_sign_flip" "vector_permute" "svd" -updateFraction 0.25 -steplr 1000 1 -print_minibatch 10 -dfname "598frame"
